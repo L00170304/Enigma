dd7078b49161014c1a9dca515c6b721cd8ee4f5f
JacekBro <95704495+JacekBro@users.noreply.github.com>
2022-01-18 13:06:07 +0000

    Update Enigma.java
    CAESAR shift added to Encrypt method

--- src/main/java/Enigma.java
+++ src/main/java/Enigma.java
@@ -10,6 +10,24 @@ public final class Enigma {
         // TODO - Implement the Encrypt method
         // Steps in brief
         // 1. Apply the CAESAR shift using the increment number
+        
+        //creating array for message characters and add characters sequentially into it with CAESAR shift applied
+        char[] ch = new char[message.length()];;
+        for (int i = 0; i < message.length(); i++) {
+            char temp = message.charAt(i);
+
+            int ascii = (int) temp; // get ASCII value of the character
+            for (int j = 0; j < incrementNumber; j++){
+
+                //90 -> 65, 122 -> 97
+                if (ascii == 90) ascii = 65; // rotate from Z to A
+                if (ascii == 122) ascii = 97; // rotate from z = a
+                if (ascii == 32) ascii = ascii - incrementNumber; //preserve blank_space
+                ascii = ascii + 1;
+            }
+            ch[i] = (char)ascii; // add shifted character of ASCII value
+        }
+        
         // 2. For each rotor in the list rotors
         //  2.1 Translate the message using the rotor
         // 3. Return the encrypted string
